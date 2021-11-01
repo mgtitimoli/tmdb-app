ff778ed01f3a762b0d0b2c4c0fc1344de83f6bd9
Mauro Gabriel Titimoli <mgtitimoli@gmail.com>
2021-11-01 16:33:59 -0300

    style: Align modules with eslint rules

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import ReactDOM from 'react-dom';
-import {StrictMode} from 'react';
+import {render} from "react-dom";
+import {StrictMode} from "react";
 
-import App from './App';
+import App from "./App";
 
-ReactDOM.render(
+render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
